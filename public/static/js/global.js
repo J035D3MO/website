0a697d29832d957f4ce299b25120afe4f427fe86
TriHydera <39857764+TriHydera@users.noreply.github.com>
2022-12-18 16:59:49 +0000

    jquery + minify

--- public/static/js/global.js
+++ public/static/js/global.js
@@ -1,8 +1,1 @@
-$.getJSON = (url, callback) => {
-  fetch(url).then(res => {
-  return res.json();
-}).then(data => callback(data)).catch(err => {
-  // Do something for an error here
-});
-}
 var urlParams = new URLSearchParams(location.search);
