23bfea166807f54ce97748ad767ae619d961475f
TriHydera <39857764+TriHydera@users.noreply.github.com>
2023-01-03 21:15:07 +0000

    Switch to w3-center class, fix redirects

--- public/page/database-list/script.js
+++ public/page/database-list/script.js
@@ -2,5 +2,5 @@ var dblist = ["browsers", "companys"];
 dblist.map(db => {
     let fL = db.charAt(0).toUpperCase(), rL = db.substring(1);
 
-    $("#content").prepend(`<button class="w3-button w3-orange"><a href="${`../../page/json-viewer/?file=${db}-db.json`}"><i class="fa-solid fa-link"></i>${fL+rL}</a></button>`);
+    $("#content").prepend(`<button class="w3-button w3-orange"><a href="../../page/json-viewer/?file=${db}-db.json"><i class="fa-solid fa-link"></i>${fL+rL}</a></button>`);
 });
