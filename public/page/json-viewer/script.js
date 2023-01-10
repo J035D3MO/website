933c768e64ef95f3195145916453a4f68f3e6198
TriHydera <39857764+TriHydera@users.noreply.github.com>
2023-01-10 15:51:50 +0000

    json viewer: using spck

--- public/page/json-viewer/script.js
+++ public/page/json-viewer/script.js
@@ -1,3 +1,18 @@
-g.fetchJSON(`../../data/${g.urlParams.get("file")}.json`, data => {
-  $("#dataview").val(data);
+let editor = new SpckEditor('#editor'), file = g.urlParams.get("file");
+
+$(".bg-green").html(`<h2>You viewing preview of "${file}"</h2>`);
+
+g.fetch(`../../data/${file}.json`, text => {
+  editor.connect()
+  .then(() => {
+
+    return editor.send({
+      project: 'Simple Project',
+      open: `${file}.json`,
+      files: [
+        {path: `${file}.json`, text: text}
+      ]
+    })
+  })
+  .catch(() => console.log('failure'));
 });
